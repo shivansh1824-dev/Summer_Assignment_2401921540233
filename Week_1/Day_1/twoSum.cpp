fa0b9154d6aba1900211a51210660964c26ebe6f
shivansh1824-dev <shivanshrai282@gmail.com>
2026-06-01 20:46:35 +0530

    Add twoSum solution for LeetCode problem
    Implement twoSum function to find indices of two numbers that add up to the target.

---
+++ Week_1/Day_1/twoSum.cpp
@@ -0,0 +1,15 @@
+//solution for leetcode
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            if(m.find(target-nums[i])!=m.end()){
+                return {m[target-nums[i]],i};
+            }
+            m[nums[i]]=i;
+        }
+        return {};
+    }
+};

f8573eca89638e334f54b85b6fe1b4d0b360993d
shivansh1824-dev <shivanshrai282@gmail.com>
2026-06-02 11:05:55 +0530

    Added maximum subarray sum solution

---
+++ Week_1/Day_2/maximumSubarraySum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// kadane's algo for maximum subarray sum
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int currSum = 0;
+        int maxSubarraySum = INT_MIN;
+        if (nums.size() == 1)
+            return nums[0];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            currSum += nums[i];
+            maxSubarraySum = max(maxSubarraySum, currSum);
+            if (currSum < 0)
+            {
+                currSum = 0;
+            }
+        }
+        return maxSubarraySum;
+    }
+};

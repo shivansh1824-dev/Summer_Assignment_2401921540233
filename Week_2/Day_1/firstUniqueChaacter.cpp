e18304983bb0bea47d2fa3985193fc6bce66934f
shivansh1824-dev <shivanshrai282@gmail.com>
2026-06-09 06:06:21 +0530

    added solution for 387

---
+++ Week_2/Day_1/firstUniqueChaacter.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//added solution for 387
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26,0);
+        for(char ch:s){
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(freq[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};

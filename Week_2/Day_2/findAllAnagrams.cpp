da23fb58819e8b180493f1b79c3a703a01f6216c
shivansh1824-dev <shivanshrai282@gmail.com>
2026-06-09 06:20:33 +0530

    added solution of 438

--- Week_2/Day_2/findAllAnagrams.cpp
+++ Week_2/Day_2/findAllAnagrams.cpp
@@ -1,4 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//
+//added solution for 438
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+        vector<int> ans;
+
+        if (p.size() > s.size()) {
+            return ans;
+        }
+
+        vector<int> pFreq(26, 0);
+        vector<int> windowFreq(26, 0);
+
+        for (char ch : p) {
+            pFreq[ch - 'a']++;
+        }
+
+        int k = p.size();
+
+        for (int i = 0; i < k; i++) {
+            windowFreq[s[i] - 'a']++;
+        }
+
+        if (windowFreq == pFreq) {
+            ans.push_back(0);
+        }
+
+        for (int i = k; i < s.size(); i++) {
+
+            windowFreq[s[i] - 'a']++;
+
+            windowFreq[s[i - k] - 'a']--;
+
+            if (windowFreq == pFreq) {
+                ans.push_back(i - k + 1);
+            }
+        }
+
+        return ans;
+    }
+};

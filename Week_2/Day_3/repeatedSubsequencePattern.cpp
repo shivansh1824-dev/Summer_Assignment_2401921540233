cac792fd2e039013947d64ae54f27fc2f0e85cb8
shivansh1824-dev <shivanshrai282@gmail.com>
2026-06-10 07:16:55 +0530

    added solution of 459

--- Week_2/Day_3/repeatedSubsequencePattern.cpp
+++ Week_2/Day_3/repeatedSubsequencePattern.cpp
@@ -1,4 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//leetcode solution of
+//leetcode solution of 459
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len != 0) continue;
+
+            string pattern = s.substr(0, len);
+            bool valid = true;
+
+            for (int i = 0; i < n; i++) {
+                if (s[i] != pattern[i % len]) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) return true;
+        }
+
+        return false;
+    }
+};
